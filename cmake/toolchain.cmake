c55cf8009914291dd63d426cb60a0d6f6d8b07ad
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-03-03 13:21:41 +0800

    cmsis dsp support

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -1,35 +1,3 @@
-function(_generate_file TARGET OUTPUT_EXTENSION OBJCOPY_BFD_OUTPUT)
-    get_target_property(TARGET_OUTPUT_NAME ${TARGET} OUTPUT_NAME)
-    if (TARGET_OUTPUT_NAME)
-        set(OUTPUT_FILE_NAME "${TARGET_OUTPUT_NAME}.${OUTPUT_EXTENSION}")
-    else()
-        set(OUTPUT_FILE_NAME "${TARGET}.${OUTPUT_EXTENSION}")
-    endif()
-
-    get_target_property(RUNTIME_OUTPUT_DIRECTORY ${TARGET} RUNTIME_OUTPUT_DIRECTORY)
-    if(RUNTIME_OUTPUT_DIRECTORY)
-        set(OUTPUT_FILE_PATH "${RUNTIME_OUTPUT_DIRECTORY}/${OUTPUT_FILE_NAME}")
-    else()
-        set(OUTPUT_FILE_PATH "${OUTPUT_FILE_NAME}")
-    endif()
-
-    add_custom_command(
-            TARGET ${TARGET}
-            POST_BUILD
-            COMMAND ${CMAKE_OBJCOPY} -O ${OBJCOPY_BFD_OUTPUT} "$<TARGET_FILE:${TARGET}>" ${OUTPUT_FILE_PATH}
-            BYPRODUCTS ${OUTPUT_FILE_PATH}
-            COMMENT "Generating ${OBJCOPY_BFD_OUTPUT} file ${OUTPUT_FILE_NAME}"
-    )
-endfunction()
-
-function(generate_binary_file TARGET)
-    _generate_file(${TARGET} "bin" "binary")
-endfunction()
-
-function(generate_hex_file TARGET)
-    _generate_file(${TARGET} "hex" "ihex")
-endfunction()
-
 set(CMAKE_EXECUTABLE_SUFFIX_C   .elf)
 set(CMAKE_EXECUTABLE_SUFFIX_CXX .elf)
 set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
@@ -37,30 +5,15 @@ set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
 # This should be safe to set for a bare-metal cross-compiler
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
+add_compile_options(-mcpu=cortex-m0 -mthumb -mthumb-interwork)
+add_compile_options(-ffunction-sections -fdata-sections -fno-common -fmessage-length=0)
+add_compile_options($<$<COMPILE_LANGUAGE:ASM>:-x$<SEMICOLON>assembler-with-cpp>)
 
-# Extra CFlags
-set(TARGET_CFLAGS_EXTRA   "-Wall -fdata-sections -ffunction-sections")
-set(TARGET_CXXFLAGS_EXTRA "-Wall -fdata-sections -ffunction-sections")
-set(TARGET_LDFLAGS_EXTRA  "-Wl,--print-memory-usage")
+# Custom linker script
+set(TARGET_LD_SCRIPT ${CMAKE_CURRENT_LIST_DIR}/n32g031_flash.ld)
+# Custom startup
+set(TARGET_STARTUP_ASM ${CMAKE_CURRENT_LIST_DIR}/startup_n32g031_gcc.s)
 
-# Device specific settings, goes to CFLAGS and LDFLAGS
-set(TARGET_CFLAGS_HARDWARE "-mcpu=cortex-m0 -mthumb")
-
-# Conditional flags
-# DEBUG
-set(CMAKE_C_FLAGS_DEBUG     "-DDEBUG=0 -O0 -g")
-set(CMAKE_CXX_FLAGS_DEBUG   "-DDEBUG=0 -O0 -g")
-set(CMAKE_ASM_FLAGS_DEBUG   "-DDEBUG=0 -O0 -g")
-
-# RELEASE
-set(CMAKE_C_FLAGS_RELEASE        "-DNDEBUG -Os") #  -flto
-set(CMAKE_CXX_FLAGS_RELEASE      "-DNDEBUG -Os") #  -flto
-set(CMAKE_ASM_FLAGS_RELEASE      "-DNDEBUG -Os") #  -flto
-#set(CMAKE_EXE_LINKER_FLAGS_RELEASE "-flto")
-
-# Final compiler flags
-set(CMAKE_C_FLAGS          "${CMAKE_C_FLAGS} ${TARGET_CFLAGS_HARDWARE} ${TARGET_CFLAGS_EXTRA}")
-set(CMAKE_CXX_FLAGS        "${CMAKE_CXX_FLAGS} ${TARGET_CFLAGS_HARDWARE} ${TARGET_CXXFLAGS_EXTRA}")
-set(CMAKE_ASM_FLAGS        "${CMAKE_ASM_FLAGS} ${CMAKE_C_FLAGS} -x assembler-with-cpp")
-set(CMAKE_EXE_LINKER_FLAGS "-specs=nosys.specs -Wl,--gc-sections ${TARGET_LDFLAGS_EXTRA}")
-
+add_link_options(-Wl,-gc-sections,--print-memory-usage,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map)
+add_link_options(-mcpu=cortex-m0 -mthumb -mthumb-interwork)
+add_link_options(-T ${TARGET_LD_SCRIPT})
