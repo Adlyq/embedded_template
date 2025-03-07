daeadbacaf1c618f000d4c0ca26b714a14eaa4f3
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-03-07 10:22:45 +0800

    Add Keil project synchronization script and update CMake configuration

--- cmake/toolchain.cmake
+++ cmake/toolchain.cmake
@@ -4,9 +4,18 @@ set(CMAKE_EXECUTABLE_SUFFIX_ASM .elf)
 
 # This should be safe to set for a bare-metal cross-compiler
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+if (CMAKE_BUILD_TYPE STREQUAL "Release")
+    set(CMAKE_CXX_FLAGS_RELEASE "${CMAKE_C_FLAGS} -O3 -Wall")
+    set(CMAKE_C_FLAGS_RELEASE "${CMAKE_C_FLAGS} -O3 -Wall")
+else ()
+    set(CMAKE_CXX_FLAGS_DEBUG "${CMAKE_C_FLAGS} -Og -g -Wall")
+    set(CMAKE_C_FLAGS_DEBUG "${CMAKE_C_FLAGS} -Og -g -Wall")
+endif ()
 
 add_compile_options(-mcpu=cortex-m0 -mthumb -mthumb-interwork)
-add_compile_options(-ffunction-sections -fdata-sections -fno-common -fmessage-length=0)
+add_compile_options(-ffunction-sections -fdata-sections -fno-common -fmessage-length=0 -fno-exceptions)
 add_compile_options($<$<COMPILE_LANGUAGE:ASM>:-x$<SEMICOLON>assembler-with-cpp>)
 
 # Custom linker script
@@ -17,3 +26,27 @@ set(TARGET_STARTUP_ASM ${CMAKE_CURRENT_LIST_DIR}/startup_n32g031_gcc.s)
 add_link_options(-Wl,-gc-sections,--print-memory-usage,-Map=${PROJECT_BINARY_DIR}/${PROJECT_NAME}.map)
 add_link_options(-mcpu=cortex-m0 -mthumb -mthumb-interwork)
 add_link_options(-T ${TARGET_LD_SCRIPT})
+
+if (NOT Python_EXECUTABLE)
+    message(FATAL_ERROR "Python executable not found")
+endif ()
+execute_process(
+        COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/cmake/isr_gen.py
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+)
+
+add_custom_target(generate_isr ALL
+        COMMAND  ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/cmake/isr_gen.py
+        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+)
+
+function(gen_keil_proj)
+    execute_process(
+            COMMAND ${Python_EXECUTABLE} ${CMAKE_SOURCE_DIR}/cmake/keil_sync.py ${ARGN}
+            WORKING_DIRECTORY ${CMAKE_HOME_DIRECTORY}
+            ERROR_VARIABLE ERROR_VAR
+    )
+    if (ERROR_VAR)
+        message(FATAL_ERROR "exec gen_keil_proj failed: ${ERROR_VAR}")
+    endif ()
+endfunction()
