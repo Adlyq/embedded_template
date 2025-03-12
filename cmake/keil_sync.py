c14949840b38f1da65b0971410aea13dd37a7ee9
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-03-12 10:03:24 +0800

    refactor: enhance path normalization and drive comparison in keil_sync.py

--- cmake/keil_sync.py
+++ cmake/keil_sync.py
@@ -1,13 +1,23 @@
 #!/usr/bin/env python3
-
 import os
 import sys
 import xml.etree.ElementTree as et
 
+def normalize_path(_path) -> str:
+    return str(os.path.normpath(_path).replace('\\', '/'))
+
+
+def same_drive(path1, path2):
+    if os.name == 'nt':  # Windows
+        return os.path.splitdrive(path1)[0] == os.path.splitdrive(path2)[0]
+    else:  # Linux and other OS
+        return True
+
+
 TARGET_NAME = 'Target_1'
 GROUP_NAME = 'Source Group 1'
-PROJECT_DIR = os.getcwd()
-KEIL_DIR = f'{os.getcwd()}/keil'
+PROJECT_DIR = normalize_path(os.getcwd())
+KEIL_DIR = normalize_path(f'{os.getcwd()}/keil')
 DEFAULT_FILE_TYPE = 9
 FILE_TYPE = {'c': 1, 'cpp': 8, 's': 2, 'h': 5, 'txt': 5}
 
@@ -19,7 +29,7 @@ for arg in sys.argv[1:]:
     elif arg == '-s':
         flag = False
     else:
-        (argInc if flag else argSrc).append(arg)
+        (argInc if flag else argSrc).append(normalize_path(arg))
 
 tree = et.parse('keil/keil.uvprojx')
 root = tree.getroot()
@@ -28,7 +38,7 @@ target = root.find(f'.//Targets/Target[TargetName="{TARGET_NAME}"]')
 incdir = target.find('.//VariousControls/IncludePath')
 
 group = target.find(f'.//Groups/Group[GroupName="{GROUP_NAME}"]')
-groupFiles = group.find('Files') if group else None
+groupFiles = group.find('Files') if group is not None else None
 if groupFiles is None:
     groupFiles = et.Element('Files')
     if group is None:
@@ -38,12 +48,13 @@ if groupFiles is None:
     group.append(groupFiles)
 
 incDirs = set(incdir.text.split(';') if incdir.text else [])
-newIncDirs = {os.path.relpath(ai, 'keil') for ai in argInc}
+newIncDirs = {os.path.relpath(ai, 'keil').replace('\\', '/') for ai in argInc}
 for d in incDirs:
-    absD = os.path.abspath(os.path.join('keil', d.replace('\\', '/')))
+    absD = normalize_path(os.path.abspath(os.path.join('keil', d)))
     if (os.path.isabs(d) or
-            os.path.commonpath([absD, PROJECT_DIR]) != PROJECT_DIR or
-            os.path.commonpath([absD, KEIL_DIR]) == KEIL_DIR):
+            not same_drive(absD, PROJECT_DIR) or
+            normalize_path(os.path.commonpath([absD, PROJECT_DIR])) != PROJECT_DIR or
+            normalize_path(os.path.commonpath([absD, KEIL_DIR])) == KEIL_DIR):
         newIncDirs.add(d)
 incdir.text = ';'.join(sorted(newIncDirs))
 
@@ -51,15 +62,16 @@ newFiles = [
     {
         'FileName': os.path.basename(asc),
         'FileType': FILE_TYPE.get(os.path.splitext(asc)[1][1:], DEFAULT_FILE_TYPE),
-        'FilePath': os.path.relpath(asc, 'keil'),
+        'FilePath': normalize_path(os.path.relpath(asc, 'keil')),
     } for asc in argSrc
 ]
 for file in groupFiles:
     path = file.find('FilePath').text
-    absPath = os.path.abspath(os.path.join('keil', path.replace('\\', '/')))
+    absPath = normalize_path(os.path.abspath(os.path.join('keil', path)))
     if (os.path.isabs(path) or
-            os.path.commonpath([absPath, PROJECT_DIR]) != PROJECT_DIR or
-            os.path.commonpath([absPath, KEIL_DIR]) == KEIL_DIR):
+            not same_drive(absPath, PROJECT_DIR) or
+            normalize_path(os.path.commonpath([absPath, PROJECT_DIR])) != PROJECT_DIR or
+            normalize_path(os.path.commonpath([absPath, KEIL_DIR])) == KEIL_DIR):
         newFiles.append({
             'FileName': file.find('./FileName').text,
             'FileType': file.find('./FileType').text,
@@ -84,8 +96,6 @@ output = f'''<?xml version="1.0" encoding="UTF-8" standalone="no" ?>
     et.tostring(ele, encoding='utf-8', xml_declaration=False, short_empty_elements=(ele.tag in ['LayerInfo', 'RTE']))
     .decode('utf-8')
     .replace(' />', '/>')
-    if ele.tag in ['LayerInfo', 'RTE'] else
-    et.tostring(ele, encoding='utf-8', xml_declaration=False).decode('utf-8')
     for ele in root.findall('*')
 ])}</Project>
 '''
