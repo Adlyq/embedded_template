d8deaf4348e1343af005bfe46b4025b567be73fc
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-03-21 15:31:38 +0800

    feat: add ring buffer implementation with header and source files

---
+++ Core/inc/ring_buffer.h
@@ -0,0 +1,51 @@
+//
+// Created by adlyq on 25-3-21.
+//
+
+#ifndef RING_BUFFER_H
+#define RING_BUFFER_H
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef __uint16_t u16;
+
+#define BUFFER_SIZE 10
+typedef int bufferType;
+
+struct RingBuffer {
+    bufferType buffer[BUFFER_SIZE];
+    u16        cursor_head;
+    u16        cursor_tail;
+    bool       isFull;
+};
+
+/**
+ * @brief get data from ring buffer
+ * @param rb RingBuffer
+ * @return pointer to data
+ */
+bufferType* RingBuffer_get(struct RingBuffer* rb);
+
+/**
+ * @brief put data into ring buffer
+ * @param rb RingBuffer
+ * @param data
+ * @return true if not full
+ */
+bool RingBuffer_put(struct RingBuffer* rb, bufferType data);
+
+/**
+ * @brief put data into ring buffer
+ * @param rb RingBuffer
+ * @param data data ptr
+ * @return true if not full
+ */
+bool RingBuffer_putPtr(struct RingBuffer* rb, const bufferType* data);
+
+/**
+ * @brief get size of ring buffer
+ * @param rb RingBuffer
+ * @return size of ring buffer
+ */
+u16 RingBuffer_size(const struct RingBuffer* rb);
+#endif //RING_BUFFER_H
