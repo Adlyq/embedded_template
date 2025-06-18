26922caf5a623447ac3039ea75413cfc5e6565ef
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-06-18 14:49:40 +0800

    feat: add project renaming script with validation and template updates

---
+++ proj_rename.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import os, sys, shutil
+
+if len(sys.argv) < 2:
+    print("Usage: python proj_rename.py <new_project_name>")
+    sys.exit(1)
+
+for char in sys.argv[1]:
+    if char not in 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_':
+        print("Invalid project name. Only alphanumeric characters and underscores are allowed.")
+        sys.exit(1)
+
+if os.path.exists('./../' + sys.argv[1]):
+    print(f"Directory '{sys.argv[1]}' already exists. Please choose a different project name.")
+    sys.exit(1)
+
+shutil.rmtree('.git')
+
+ctx_list = []
+for path, dirs, files in os.walk('./.idea'):
+    for file in files:
+        if file.find('embedded-template') != -1:
+            ctx_list.append((path, file))
+        with open(os.path.join(path, file), 'r', encoding='utf-8') as f:
+            content = f.read().replace('n32g031_template', sys.argv[1]).replace('embedded-template', sys.argv[1]).replace("embedded_template", sys.argv[1])
+        with open(os.path.join(path, file), 'w', encoding='utf-8') as f:
+            f.write(content)
+    for _dir in dirs:
+        if _dir.find('embedded-template') != -1:
+            ctx_list.append((path, _dir))
+
+
+for path, file in ctx_list:
+    os.renames(os.path.join(path, file), os.path.join(path, file.replace('embedded-template', sys.argv[1])))
+
+with open('./CMakeLists.txt', 'r', encoding='utf-8') as f:
+    content = f.read().replace('n32g031_template', sys.argv[1])
+with open('./CMakeLists.txt', 'w', encoding='utf-8') as f:
+    f.write(content)
+
+os.renames(os.getcwd(), os.path.abspath('../' + sys.argv[1]))
+os.remove('proj_rename.py')
