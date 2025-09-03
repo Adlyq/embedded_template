06e79f684133ede7ea81c8a864d6abff7b4207a3
adlyq <45322943+Adlyq@users.noreply.github.com>
2025-09-03 14:38:08 +0800

    fix(output): 修正输出端口状态设置逻辑

--- Core/src/output.c
+++ Core/src/output.c
@@ -90,12 +90,12 @@ void outputSet(const bool state) {
 #endif
         GPIO_SetBits(OUTPUT_LO_PORT, OUTPUT_LO_PIN);
 #ifdef OUTPUT_DO_PORT
-        GPIO_SetBits(OUTPUT_DO_PORT, OUTPUT_DO_PIN);
+        GPIO_ResetBits(OUTPUT_DO_PORT, OUTPUT_DO_PIN);
 #endif
     } else {
         GPIO_ResetBits(OUTPUT_LO_PORT, OUTPUT_LO_PIN);
 #ifdef OUTPUT_DO_PORT
-        GPIO_ResetBits(OUTPUT_DO_PORT, OUTPUT_DO_PIN);
+        GPIO_SetBits(OUTPUT_DO_PORT, OUTPUT_DO_PIN);
 #endif
     }
 
